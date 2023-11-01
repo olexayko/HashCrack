775cedc612d50eab090a44d99a2124194813dd56
olexayko <95551086+olexayko@users.noreply.github.com>
2023-11-01 02:19:49 +0200

    Add Progress Bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     //QObject::connect(this, &MainWindow::stopWorkerSignal, this, &MainWindow::stop_worker);
 }
 
@@ -31,61 +32,61 @@ QString SHA1(QString input)
     return sha1Hash.toHex();
 }
 
+
 int thr_num = 0;
-void MainWindow::launch_worker(long long start_val, long long end_val, int operations_amount, int thread_number = thr_num++)
+int ui_update_frequency = 10000000;
+
+void MainWindow::sha1_worker(long long start_val, long long end_val, int thread_number = thr_num++)
 {
     QString true_hash = ui->lineEdit->text();
     QString phone_prefix = "+";
 
     QElapsedTimer timer;
     timer.start();
-    //qDebug() << "Start seconds: " + QString::number(int(timer.nsecsElapsed() / 1000000000.0));
 
     long long val = start_val;
     while (val < end_val)
     {
         QString input = phone_prefix + QString::number(val);
         QString result_hash = SHA1(input);
-        val++;
 
         if(result_hash == true_hash)
         {
-            ui->textEdit->append("FOUND TRUE NUMBER: " + input + " SHA1 = " + result_hash);
-            ui->textEdit->append("Exiting thread # " + QString::number(thread_number) + " after " + QString::number(int(timer.nsecsElapsed() / 1000000000.0)) + " seconds");
-            return;
+            ui->textEdit->append("Found possible number: " + input + " with SHA1 = " + result_hash);
         }
 
-        if(val % 10000000 == 0)
+        if(val % ui_update_frequency == 0)
         {
-            qDebug() << "Number: " + input + " SHA1 = " + result_hash;
+            emit workDone();
         }
+        val++;
     }
-
-    //qDebug() << "End seconds: " + QString::number(int(timer.nsecsElapsed() / 1000000000.0));
-    //ui->textEdit->append("Elapsed time (s): " + QString::number(int(timer.nsecsElapsed() / 1000000000.0)));
     ui->textEdit->append("Exiting thread # " + QString::number(thread_number) + " after " + QString::number(int(timer.nsecsElapsed() / 1000000000.0)) + " seconds");
 }
 
-
-QMutex mutex;
 vector<QThread*> worker_threads {};
 
-
-void MainWindow::launch_sha_calc(int threads_number = QThread::idealThreadCount())
+void MainWindow::launch_sha1_calc(int threads_number = QThread::idealThreadCount())
 {
     long long starting_phone = 380000000000;
     long long final_phone =    381000000000;
     long long operations_amount = final_phone - starting_phone;
     long long operations_amount_per_thread = operations_amount / threads_number + 1;
+
+    ui->progressBar->setMaximum(operations_amount);
+
+    connect(this, &MainWindow::workDone, this, &MainWindow::updateUI);
+
     for(int i = 0; i < threads_number; i++)
     {
         long long start_val = starting_phone + i * operations_amount_per_thread;
         long long end_val = start_val + operations_amount_per_thread;
-        //qDebug() << "vals: " << start_val << " " << end_val << "thr num: " << threads_number;
+
         QThread* thread = new QThread;
         QObject::connect(thread, &QThread::started, [=] {
-            launch_worker(start_val, end_val, operations_amount_per_thread);
+            sha1_worker(start_val, end_val);
         });
+
         worker_threads.push_back(thread);
         for (int i = 0; i < worker_threads.size(); i++)
         {
@@ -97,7 +98,12 @@ void MainWindow::launch_sha_calc(int threads_number = QThread::idealThreadCount(
 
 void MainWindow::on_pushButton_clicked()
 {
-    launch_sha_calc();
+    launch_sha1_calc();
+}
+
+void MainWindow::updateUI()
+{
+    ui->progressBar->setValue(ui->progressBar->value() + ui_update_frequency);
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -16,12 +16,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void launch_worker(long long start_index, long long end_index, int operations_amount, int thread_number);
-    void launch_sha_calc(int threads_number);
+    void sha1_worker(long long start_index, long long end_index, int thread_number);
+    void launch_sha1_calc(int threads_number);
 
 private slots:
-
     void on_pushButton_clicked();
+
+public slots:
+    void updateUI();
+
+signals:
+    void workDone();
 
 private:
     Ui::MainWindow *ui;
